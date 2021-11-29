317b1e1e78afb7d47d5694ef31e3ca72acab864a
Kunal Nagdev <nagdevkunal2@gmail.com>
2021-11-29 05:21:40 -0600

    added images upload and view fixes

--- src/Components/Auth/Modules/Chats/index.js
+++ src/Components/Auth/Modules/Chats/index.js
@@ -4,12 +4,15 @@ import io from "socket.io-client";
 import { connect } from 'react-redux';
 import { isEmpty, isEqual, map } from "lodash";
 import { bindActionCreators } from "redux";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from  'react-bootstrap/Tooltip';
 
 import Camera from '../../../../Assets/camera.png'
-import { chatBaseURL } from "../../../../Utils/baseUrl";
+import baseURL, { chatBaseURL } from "../../../../Utils/baseUrl";
 import "../../index.css"
 import { showAllCustomersOperation } from '../../../../State/Customers/operations';
 import { getConversationOperation, getAllMessagesOperations, saveMessageOperations } from "../../../../State/Chats/operations";
+import { Spinner } from "react-bootstrap";
 
 const Chats = ({ currentUser, isManager, actions, manager }) => {
 	const [ state, setState ] = useState({ message: "", name: currentUser.name })
@@ -18,7 +21,9 @@ const Chats = ({ currentUser, isManager, actions, manager }) => {
 	const [receiver, setReceiver] = useState(isManager ? {} : manager);
 	const [allCustomers, setAllCustomers] = useState([]);
 	const [conversationId, setConversationId] = useState(0);
-
+	const [isLoading, setLoading] = useState(false);
+	const [img, setImg] = useState({});
+	
 	useEffect(
 		() => {
 			if (!isManager) getConversation(manager);
@@ -27,13 +32,27 @@ const Chats = ({ currentUser, isManager, actions, manager }) => {
 				setAllCustomers(res);
 			})
 			socketRef.current = io.connect(chatBaseURL)
-			socketRef.current.on("message", ({ name, message }) => {
-				setChat([ ...chat, { name, message } ])
+			const date = new Date();
+			const timestamp = date.getTime();
+			const author = currentUser.id;
+			socketRef.current.on("message", ({  message, isFile, author, conversationId, timestamp }) => {
+				
+				getAllMessagesF(conversationId);
 			})
 			return () => socketRef.current.disconnect()
 		},
 		[]
 	)
+
+	useEffect(() => { 
+		uploadImg();
+	}, [img]);
+
+
+	const scrollToBottom = () => {
+		const element = document.getElementById('chat-screen');
+		element.scrollTop = element.scrollHeight - element.clientHeight;
+	}
 
 	const onTextChange = (e) => {
 		setState({ ...state, [e.target.name]: e.target.value })
@@ -41,43 +60,89 @@ const Chats = ({ currentUser, isManager, actions, manager }) => {
 
 	const onMessageSubmit = (e) => {
 		const { name, message } = state;
-		if (message) {
-			socketRef.current.emit("message", { name, message });
+		if (message.trim().length > 0) {
+			const date = new Date();
+			socketRef.current.emit("message", { message, isFile: false, timestamp:date.getTime(), author: currentUser.id, conversationId });
 			e.preventDefault();
 			setState({ message: "", name });
 			const msgObj = { author: parseInt(currentUser.id, 10), message, conversationId, isFile: false };
 			actions.saveMessageOperations(msgObj)
 			.then((res) => {
-				const date = new Date();
 				msgObj['timestamp'] = date.getTime();
 				if (res) setChat([...chat, msgObj])
+				scrollToBottom();
 			})
-		} else {
-
 		}
 	}
 
+	const uploadImg = () => {
+		if (img.name) {
+			const formData = new FormData();
+			formData.append('img', img);
+			setLoading(true);
+			fetch(baseURL+"/api.php", {
+				method: 'POST',
+				body: formData
+			})
+			.then((res) => res.json())
+			.then((resp) => {
+				setLoading(false);
+				socketRef.current.emit("message", { name: currentUser.name, message: resp.url, isFile:true });
+				const msgObj = { author: parseInt(currentUser.id, 10), message:resp.url, conversationId, isFile: true };
+				actions.saveMessageOperations(msgObj)
+				.then((res) => {
+					const date = new Date();
+					msgObj['timestamp'] = date.getTime();
+					if (res) setChat([...chat, msgObj]);
+					scrollToBottom();
+				})
+			})
+		} 
+  }
 
 	const getConversation = (receiver) => {
 		setReceiver(receiver);
 		actions.getConversationOperation({ senderId: currentUser.id, receiverId: receiver.id })
 		.then((res) => {
-			setConversationId(parseInt(res.id, 10));
-			actions.getAllMessagesOperations({ conversationId: parseInt(res.id, 10) })
-			.then((resp) => {
-				setChat(resp);
-			})
+			getAllMessagesF(parseInt(res.id, 10));
 		})
 	}
 
+	const getAllMessagesF = (id) => {
+		setConversationId(id);
+		actions.getAllMessagesOperations({ conversationId: id })
+		.then((resp) => {
+			setChat(resp);
+			scrollToBottom();
+		})
+	}
+
+	const renderTooltip = (props) => (
+		<Tooltip id="button-tooltip" {...props}>
+			Open image in new tab.
+		</Tooltip>
+	);
+
 	const renderChat = () => {
-		return chat.map(({ author, name, message, timestamp }, index) => {
+		return chat.map(({ author, name, message, timestamp, isFile, conversationId }, index) => {
 			const date = new Date(parseInt(timestamp, 10));
 			return (isEqual(name, currentUser.name) || isEqual(author, parseInt(currentUser.id, 10))) ? (
           <div className="sender" key={`${index}-msg`}>
             <div className="user-msg">
 	            <h4 className="user-name">{(index > 0 && !isEqual(chat[index - 1].author, parseInt(currentUser.id, 10)) || isEqual(index, 0)) && currentUser.name}</h4>							
-              <p className="message">{message}</p>
+              {isEqual(isFile, 'true') ? (
+								<OverlayTrigger
+									placement="bottom"
+									delay={{ show: 250, hide: 400 }}
+									overlay={renderTooltip}
+								>
+									<button onClick={() => window.open(message)}>
+										<img src={message} alt="img" className="msg-img" />
+									</button>
+								</OverlayTrigger>
+							) : (
+								<p className="message">{message}</p>
+							)}
 							<span className="msg-time">{date.toLocaleString()}</span>
             </div>
 						{(index > 0 && !isEqual(chat[index - 1].author, parseInt(currentUser.id, 10)) || isEqual(index, 0)) ? (
@@ -91,9 +156,20 @@ const Chats = ({ currentUser, isManager, actions, manager }) => {
 						) : <div className='profile-image-div' />}
             <div className="user-msg">
 							<h4 className="user-name">{(index > 0 && !isEqual(chat[index - 1].author, parseInt(receiver.id, 10))|| isEqual(index, 0)) && receiver.name}</h4>
-              <p className="message">{message}</p>
+							{isEqual(isFile, 'true') ? (
+								<OverlayTrigger
+									placement="bottom"
+									delay={{ show: 250, hide: 400 }}
+									overlay={renderTooltip}
+								>
+									<button onClick={() => window.open(message)}>
+										<img src={message} alt="img" className="msg-img" />
+									</button>
+								</OverlayTrigger>
+							) : (
+								<p className="message">{message}</p>
+							)}
 							<span className="msg-time">{date.toLocaleString()}</span>
-
             </div>
           </div>
 		)})
@@ -111,12 +187,11 @@ const Chats = ({ currentUser, isManager, actions, manager }) => {
 					</li>
 					))}
 			</ul>)}
-			<div className="chat-screen">
+			<div className="chat-screen" id="chat-screen">
 				{isEmpty(receiver) ? (
 					<h3> Select Customer to chat </h3>
 				) : (
 					<form onSubmit={onMessageSubmit}>
-						<h3> Connect with {receiver.name.toUpperCase()} {receiver.type && `(${receiver.type.toUpperCase()})`} </h3>
 						<div>
 							{renderChat()}
 						</div>
@@ -131,11 +206,18 @@ const Chats = ({ currentUser, isManager, actions, manager }) => {
 							/>
 							{state.message ? (
 								<button className="message-snd-btn">Send</button>
-
 							) : (
-								<button className="message-snd-btn">
-									<img src={Camera} alt="camera" className="camera-btn" />
-								</button>
+									<label className="message-snd-btn">
+										{isLoading ? (
+											<Spinner />
+										) : (
+											<>
+												<input type="file" accept="image/*" name="img" onChange={(e) => setImg(e.target.files[0])} />
+												<img src={Camera} alt="camera" className="camera-btn" />
+											</>
+										)}
+										
+									</label>
 							)}
 						</div>
 					</form>
